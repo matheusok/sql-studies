758893d933fb9c207731d7c9ad6cd341c88a3bfc
Matheus Oliveira <okmatheusoliveira@gmail.com>
2023-02-04 06:28:15 -0300

    Added SELECT queries.

---
+++ select-queries.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM Person.Person;
+
+SELECT Title
+FROM Person.Person;
+
+SELECT *
+FROM Person.EmailAddress;
+
+/*
+The Marketing team needs to do a search
+on the most common names of its customers
+and needs the first and last names of all customers
+who are registered in the system.
+*/
+SELECT FirstName, LastName
+FROM Person.Person;
